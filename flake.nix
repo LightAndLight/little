4c83be8324c4761c08afc66bb5125ef2ab79c6b7
Isaac Elliott <isaace71295@gmail.com>
2024-03-31 07:03:26 +1000

    Initial commit for Haskell embedding

--- flake.nix
+++ flake.nix
@@ -4,33 +4,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = import nixpkgs { 
-      inherit system; 
-      overlays = [
-        (self: super: {
-	  haskell = super.haskell // {
-	    packages = super.haskell.packages // {
-	      ghc923 = super.haskell.packages.ghc923.override {
-	        overrides = new: old: {
-	          mutable-containers = super.haskell.lib.dontCheck old.mutable-containers;
-		};
-	      };
-	    };
-	  };
-	})
-      ];
-    };
-    in rec {
+    let pkgs = import nixpkgs { inherit system; }; in 
+    {
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [
-          haskell.packages.ghc923.ghc
+          haskellPackages.ghc
           cabal-install
-          (haskell-language-server.override { supportedGhcVersions = [ "923" ]; })
-	  pandoc
-          cabal2nix
+          haskell-language-server
         ];
       };
-      packages.little = pkgs.haskell.packages.ghc923.callPackage ./little.nix {};
-      defaultPackage = packages.little;
-  });
+    });
 }
